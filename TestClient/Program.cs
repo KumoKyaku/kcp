9a20325fd963db4479557c278685210a1c38b6a9
云却 <479813005@qq.com>
2021-09-16 12:36:50 +0800

    增加简单例子

---
+++ TestClient/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press F1 send word.......");
+            UdpClient client = new UdpClient();
+
+            while (true)
+            {
+                var k = Console.ReadKey();
+                if (k.Key == ConsoleKey.F1)
+                {
+                    Send(client, "发送一条消息");
+                }
+            }
+
+            Console.ReadLine();
+        }
+
+        static IPEndPoint end = new System.Net.IPEndPoint(System.Net.IPAddress.Loopback, 40001);
+        static async void Send(UdpClient client, string v)
+        {
+            var buffer = System.Text.Encoding.UTF8.GetBytes(v);
+            client.SendAsync(buffer, buffer.Length, end);
+            var resp = await client.ReceiveAsync();
+            var respstr = System.Text.Encoding.UTF8.GetString(resp.Buffer);
+            Console.WriteLine($"收到服务器回复:    {respstr}");
+        }
+    }
+}
---
+++ TestServer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace TestServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            UdpClient client = new UdpClient(40001);
+            StartRecv(client);
+            Console.ReadLine();
+        }
+
+        static async void StartRecv(UdpClient client)
+        {
+            var res = await client.ReceiveAsync();
+            StartRecv(client);
+
+            await Task.Delay(1);
+            var str = System.Text.Encoding.UTF8.GetString(res.Buffer);
+            Console.WriteLine(str);
+            client.SendAsync(res.Buffer, res.Buffer.Length, res.RemoteEndPoint);
+        }
+
+    }
+}
