8612d8dfdac5d6014c721922e4b37e354e6fc96a
云却 <479813005@qq.com>
2022-10-19 19:57:18 +0800

    使用内存池优化KcpIO

--- Kcp/KcpIO.cs
+++ Kcp/KcpIO.cs
@@ -221,6 +221,11 @@ namespace System.Net.Sockets.Kcp
             Owner.Dispose();
         }
 
+        protected internal override BufferOwner CreateBuffer(int needSize)
+        {
+            return MemoryPool<byte>.Shared.Rent(needSize);
+        }
+
         internal class OutputQ : QueuePipe<(BufferOwner Owner, int Count)>,
             IKcpCallback
         {
